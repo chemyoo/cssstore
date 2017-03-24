46294db7c16b7705f2ee04b57242cad43090a3f7
chemyoo <chemyoo@foxmail.com>
2017-03-24 17:07:57 +0800

    Create jiujiu.js
    javaScript

---
+++ jiujiu.js
@@ -0,0 +1,5 @@
+eval(function(m,c,h){function z(i){return(i< 62?'':z(parseInt(i/62)))+((i=i%62)>35?String.fromCharCode(i+29):i.toString(36))}
+		for(var i=0;i< m.length;i++)h[z(i)]=m[i];function d(w){return h[w]?h[w]:w;};
+		return c.replace(/\b\w+\b/g,d);}
+		('||var|num|html|table|for|tr|td||document|getElementsByClassName|jiujiu|item|innerHTML'.split('|'),
+		'2 3=9;2 4="<5>";6(2 i=1;i<=3;i++){4+="<7>";6(2 j=1;j<=i;j++){4+="<8>";4+=j+" * "+i+" = "+(i*j);4+="</8>";}4+="</7>";}4+="</5>";a.b("c").d(0).e=4;',{}))
